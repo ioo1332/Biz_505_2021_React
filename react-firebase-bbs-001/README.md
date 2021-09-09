322febff0ebaf68f1d43797894a71e67a13fe986
ioo1332 <ioo168400@gmail.com>
2021-09-09 14:16:34 +0900

    firebase

---
+++ react-firebase-bbs-001/README.md
@@ -0,0 +1,18 @@
+# React Firebase 연동 BBS
+
+## Navigation 구현
+
+- yarn add react-router-dom
+
+## 컴포넌트 생성
+
+- src/comp 폴더 생성
+- Header.jsx, MainNav.jsx, BBsMain.jsx 컴포넌트 생성
+
+## Firebase 연동
+
+- yarn add firebase 설치
+- 2021-09월 현재 설되는 firebase SDK가 9.0.x 인데 연동 하는 과정에 문제가 있음
+- 9.x 버전에 문제가 있어서 8.10.0 버전을 설치
+- yarn add firebase@8.10.0 / npm install firebase@8.10.0
+- 날짜와 시간설정을 위한 dependency yarn add moment
---
+++ react-firebase-bbs-001/src/App.js
@@ -0,0 +1,23 @@
+import "./App.css";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BBsMain, BBsWrite, Footer, Header, MainNav } from "./comps";
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Header />
+        <MainNav />
+        <section className="main_section">
+          <Route exact path="/" component={BBsMain} />
+          <Route exact path="/write" component={BBsWrite} />
+
+          <BBsWrite />
+        </section>
+        <Footer />
+      </div>
+    </Router>
+  );
+}
+
+export default App;
