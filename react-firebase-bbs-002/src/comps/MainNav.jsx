877622c4384574fb5a8f40176477c5b05c8e28c7
ioo1332 <ioo168400@gmail.com>
2021-09-10 15:41:22 +0900

    게시판,firebase

---
+++ react-firebase-bbs-002/src/comps/MainNav.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import "../css/MainNav.css";
+import { BrowserRouter, NavLink } from "react-router-dom";
+
+function MainNav() {
+  return (
+    <BrowserRouter>
+      <ul className="main_nav">
+        <li>
+          <NavLink exact to="/" activeClassName="active_nav">
+            HOME
+          </NavLink>
+        </li>
+        <li>
+          <NavLink exact to="/write" activeClassName="active_nav">
+            글쓰기
+          </NavLink>
+        </li>
+        <li>
+          <NavLink exact to="/login" activeClassName="active_nav">
+            로그인
+          </NavLink>
+        </li>
+        <li>
+          <NavLink exact to="/join" activeClassName="active_nav">
+            회원가입
+          </NavLink>
+        </li>
+      </ul>
+    </BrowserRouter>
+  );
+}
+
+export default MainNav;
